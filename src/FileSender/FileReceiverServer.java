ce8b13e766579da30e965cdfc0a64ced16e5c36d
MohamedAmineElkhayya <amineelkhayyar8@gmail.com>
2024-02-06 12:39:04 +0100

    DIA 06/02/2024

---
+++ src/FileSender/FileReceiverServer.java
@@ -0,0 +1,25 @@
+package FileSender;
+import java.io.*;
+        import java.net.*;
+
+public class FileReceiverServer {
+     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(2020);
+             Socket socket = serverSocket.accept();
+             InputStream inputStream = socket.getInputStream();
+             FileOutputStream fileOutputStream = new FileOutputStream("src/filesReceived/MySecretFileReceived.txt")) {
+
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+            }
+
+            System.out.println("Archivo recibido con éxito.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
